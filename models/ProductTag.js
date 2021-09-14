67b83574611698c306dcf3f8e6626cbaf09e7db4
Andrew Bwogi <andrew.bwogi@gmail.com>
2021-09-14 01:40:02 -0700

    database associations in the model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,26 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      referneces: {
+        model: "product",
+        key: "id",
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      referneces: {
+        model: "tag",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
